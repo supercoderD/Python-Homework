9ec26cd181e665b2fd442dad6a97592702483d6e
supercoderD <darshugandhi1@gmail.com>
2026-01-16 20:36:32 -0800

    initialcommit

---
+++ temperature.py
@@ -0,0 +1,12 @@
+name=input("What's your name?")
+temp=int(input("Enter the degrees outside. :"))
+if temp<60:
+    print("You will need to wear heavy suiting", name)
+else:
+    print("You will need to wear light clothes", name)
+
+
+print("I know that your name is", name)
+print("and that the temperature outside is", temp)
+print("Thanks for your time,and have a good day!")
+
