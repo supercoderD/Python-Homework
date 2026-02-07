3de2ef43d28c6371e2ce108695d95481c25c3e87
supercoderD <darshugandhi1@gmail.com>
2026-02-06 18:33:02 -0800

    initial commit

---
+++ swapping.py
@@ -0,0 +1,23 @@
+a=100
+b=200
+a=200
+b=100
+print("a is 100")
+print("b is 200")
+if a == 200:
+    print("a is",  a) 
+    print("You have successfully swapped variables.")
+else:
+    a=200
+    print("a is",  a)
+    print("You have successfully swapped variables.")
+
+if b == 100:
+    print("b is",  b)
+    print("You have successfully swapped variables.")
+else:
+    b=100
+    print("b is",  b)
+    print("You have successfully swapped variables.")
+
+
