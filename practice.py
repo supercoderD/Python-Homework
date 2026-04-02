e8b33eb22b059d5c29afe191e594a1f6194d9e1e
supercoderD <darshugandhi1@gmail.com>
2026-04-01 19:56:43 -0700

    initialcommit

---
+++ practice.py
@@ -0,0 +1,24 @@
+x=int(input("Enter a number : "))
+if x%2==0:
+    print("Your number is even.")
+else:
+    print("Your number is odd.")
+
+age=int(input("Enter a random age: "))
+if age<13:
+    print("Child.")
+elif age<20 and age>13:
+    print("Tenager")
+elif age>20 and age<60:
+    print("Adult")
+else: 
+    print("Senior")
+
+x=2
+
+    
+
+
+
+
+
---
+++ turtless.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.Screen().bgcolor('green')
+turtle.Screen().setup(400,300)
+t=turtle.Turtle()
+t.speed(5)
+#Square Spiral Pattern
+for i in range(4):
+    t.forward(100)
+    t.right(90)
+turtle.done()
