5df2705d2308a721630b928995a0070f13740eb1
supercoderD <darshugandhi1@gmail.com>
2026-06-09 12:51:51 -0700

    initial commit

--- lists[]homework.py
+++ lists[]homework.py
@@ -1,10 +1,13 @@
 x=[]
+y=[]
 start=int(input("Enter a number."))
 end=int(input("Enter a number."))
 for i in range(start, end+1):
     y=i**2
-    x.append(y)
-print(x)                                     
+    if x%2==0:
+        x.append(y)
+
+
 
 
 
---
+++ tuples()homework.py
@@ -0,0 +1,10 @@
+tuple1=(4,3,2,2,-1,18)
+tuple2=(2,4,8,8,3,2,9)
+product=1
+product2=1
+for item in tuple1: 
+    product=product*item
+for item in tuple2: 
+    product2=product2*item
+print("The product of all the numbers in tuple1 is:", product)
+print("The product of all the numbers in tuple2 is", product2)
